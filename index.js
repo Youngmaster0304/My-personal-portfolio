2a6a8ff8bc6b956bf5e30530ebb0c7c0c7af0c0c
Abhinav Jha <Abhinavjha0304@gmail.com>
2025-11-04 16:36:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,21 +3,27 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.statements = exports.statement = exports.smart = exports.program = exports.expression = exports.default = void 0;
-var formatters = require("./formatters.js");
-var _builder = require("./builder.js");
-const smart = exports.smart = (0, _builder.default)(formatters.smart);
-const statement = exports.statement = (0, _builder.default)(formatters.statement);
-const statements = exports.statements = (0, _builder.default)(formatters.statements);
-const expression = exports.expression = (0, _builder.default)(formatters.expression);
-const program = exports.program = (0, _builder.default)(formatters.program);
-var _default = exports.default = Object.assign(smart.bind(undefined), {
-  smart,
-  statement,
-  statements,
-  expression,
-  program,
-  ast: smart.ast
+var _lowercase = require("./lowercase.js");
+Object.keys(_lowercase).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _lowercase[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _lowercase[key];
+    }
+  });
+});
+var _uppercase = require("./uppercase.js");
+Object.keys(_uppercase).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _uppercase[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _uppercase[key];
+    }
+  });
 });
 
 //# sourceMappingURL=index.js.map
